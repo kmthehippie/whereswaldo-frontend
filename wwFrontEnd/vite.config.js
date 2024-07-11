4b47c5c67f9f8690399d4bea6080c1b27312be45
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-07-11 21:26:41 +0800

    public/assets @defineConfig

--- wwFrontEnd/vite.config.js
+++ wwFrontEnd/vite.config.js
@@ -7,5 +7,5 @@ import  { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [react(), VitePWA(), copy({targets:[{src:'public/_redirects',  dest: 'dist'}]})],
   build: {outDir: 'dist'},
-  publicDir: "assets"
+  publicDir: "public/assets"
 })
