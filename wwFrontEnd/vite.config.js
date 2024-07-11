910874e171456d5ffe6d99ad4ba6089b60a76e77
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2024-07-11 21:01:46 +0800

    Add vite-plugin-pwa

--- wwFrontEnd/vite.config.js
+++ wwFrontEnd/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import copy from "rollup-plugin-copy"
+import  { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), copy({targets:[{src:'public/_redirects', dest: 'dist'}]})],
+  plugins: [react(), VitePWA(), copy({targets:[{src:'public/_redirects',  dest: 'dist'}]})],
   build: {outDir: 'dist'}
 })
